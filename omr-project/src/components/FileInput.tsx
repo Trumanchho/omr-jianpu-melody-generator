30bdf104bb1176f0e135885530d8abc0db456d10
truman <trumanchho@gmail.com>
2025-02-05 16:11:55 -0800

    started file upload component

---
+++ omr-project/src/components/FileInput.tsx
@@ -0,0 +1,11 @@
+
+function FileInput() {
+    return (
+        <div>
+            <label htmlFor="myfile">Upload File:</label>
+            <input type="file" id="myfile" name="myfile" />
+        </div>
+    );
+}
+
+export default FileInput;
