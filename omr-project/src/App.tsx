fc373d892714332352c214ce98f3ba930756fee0
truman <trumanchho@gmail.com>
2025-02-22 00:22:20 -0800

    added styles

--- omr-project/src/App.tsx
+++ omr-project/src/App.tsx
@@ -3,21 +3,27 @@ import ImageList from './components/ImageList';
 import CharGridProvider from './contexts/charGridContext';
 import TokensProvider from './contexts/tokenContext';
 import IsPredictingProvider from './contexts/isPredictingContext';
+import {Helmet} from 'react-helmet'
 
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <div id='main'>
+      <Helmet>
+          <style>{'body { background-color: rgb(50, 50, 50); }'}</style>
+      </Helmet>
       <h1>Jianpu Scanner</h1>
-      <TokensProvider>
-      <CharGridProvider>
-      <IsPredictingProvider>
-        <ImageList/>
-        <CharGrid/>
-      </IsPredictingProvider>
-      </CharGridProvider>
-      </TokensProvider>
+      <div>
+        <TokensProvider>
+        <CharGridProvider>
+        <IsPredictingProvider>
+          <ImageList/>
+          <CharGrid/>
+        </IsPredictingProvider>
+        </CharGridProvider>
+        </TokensProvider>
+      </div>
     </div>
   );
 }
